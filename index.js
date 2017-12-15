c3e5fadcf2a5416ab826cb63d9f896c7601d55db
msalam <ms17alam@gmail.com>
2017-12-15 15:12:12 -0500

    alphabate tunes works

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-function playingSound (e){
+
+function playSound(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-  if(!audio) return;
+  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  if (!audio) return;
+
+  key.classList.add('playing');
   audio.currentTime = 0;
   audio.play();
-  key.classList.add('playing');
-}
-function removeTransition(e){
-  if(event.propertyName !== 'transform') return;
-  this.classLIst.remove('playing');
 }
 
-const keys = document.querySelectorAll('.key');
-keys.forEach(key => key.addEventListener('transitioned', removeTransition));
-window.addEventListener('keydown', playSound)
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing');
+  }
+
+  const keys = Array.from(document.querySelectorAll('.key'));
+  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+  window.addEventListener('keydown', playSound);
